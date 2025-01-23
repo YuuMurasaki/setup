0ff9e35e4912e30dd6aba61367e19cea53325244
YuuMurasaki <hoangthibaongoc411@gmail.com>
2025-01-23 09:24:50 +0700

    hehe

--- install-mint.sh
+++ install-mint.sh
@@ -3,7 +3,6 @@
 # Update system and repositories, add repos
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
-wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt-get update
 
@@ -19,7 +18,6 @@ sudo apt-get install -y libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxext-dev l
 sudo apt-get install -y tcc gcc build-essential gdb clang clang-tools clang-format clang-tidy
 sudo apt-get install -y clang clang-tools clang-format clang-tidy llvm lldb
 sudo apt-get install -y ninja-build gettext make cmake unzip curl wget
-sudo apt-get install -y virtualbox-7.1
 
 # Cli tools
 sudo apt-get install -y wezterm
@@ -30,9 +28,10 @@ sudo apt-get install -y mpv nsxiv zathura zathura-poppler
 sudo apt-get install -y maim imagemagick
 
 # Vietnamese
-sudo apt-get install -y fcitx5 fcitx5-unikey fcitx5-configtool
+#sudo apt-get install -y fcitx5 fcitx5-unikey fcitx5-config-qt
 
 # Install fonts
+sudo apt-get install -y ubuntu-restricted-extras
 sudo apt-get install -y fonts-recommended
 sudo apt-get install -y fonts-hack fonts-firacode fonts-jetbrains-mono
 sudo apt-get install -y fonts-font-awesome
@@ -60,8 +59,8 @@ git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 ### Building neovim
 cd $HOME/Downloads
 git clone https://github.com/neovim/neovim
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
+#cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+#sudo make install
 
 # Mullvad browser
 wget --trust-server-names https://mullvad.net/en/download/browser/linux-x86_64/latest -P ~/Downloads
