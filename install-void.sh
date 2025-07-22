4855b17aac28a9238023bc2e45d04c84fa584616
Yuu Murasaki <hoangthibaongoc411@gmail.com>
2025-07-22 22:23:25 +0700

    remove unused packages
    trash-cli remove because I use rm
    tcc remove because I don't do C programing

--- install-void.sh
+++ install-void.sh
@@ -7,10 +7,8 @@
 # -session    optional    pam_rundir.so
 ###################################################################################
 # Setup pipewire
-# mkdir -p /etc/pipewire/pipewire.conf.d
 # ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 # ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
-# mkdir -p /etc/alsa/conf.d
 # ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
 # ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
 ###################################################################################
@@ -25,12 +23,11 @@ xbps-install -y xorg xinit xrandr xsetroot xcompmgr xdotool xset xclip xwallpape
 xbps-install -y vulkan-loader mesa-vulkan-intel unclutter-xfixes
 xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel libXrandr-devel ncurses
 xbps-install -y freetype-devel fontconfig-devel xdg-utils xdg-user-dirs ntfs-3g
-xbps-install -y dbus dbus-x11 pam_rundir NetworkManager ImageMagick ffmpeg gnupg pinentry-gtk
+xbps-install -y dbus dbus-x11 pam_rundir NetworkManager gnupg pinentry-gtk
 xbps-install -y pipewire wireplumber rtkit alsa-pipewire
-xbps-install -y git oath-toolkit make curl wget patch
-xbps-install -y lua luarocks LuaJIT gcc tcc
-xbps-install -y stow htop slop zathura-pdf-poppler
-xbps-install -y fzf eza trash-cli rsync tar zip unzip 7zip
+xbps-install -y git oath-toolkit make curl wget patch gcc lua luarocks LuaJIT
+xbps-install -y stow htop slop zathura-pdf-poppler ImageMagick ffmpeg
+xbps-install -y fzf eza rsync tar zip unzip 7zip
 xbps-install -y font-iosevka freefont-ttf dejavu-fonts-ttf ttf-ubuntu-font-family
 xbps-install -y liberation-fonts-ttf font-libertine-ttf font-awesome6
 xbps-install -y noto-fonts-emoji noto-fonts-cjk noto-fonts-cjk-variable noto-fonts-ttf noto-fonts-ttf-extra noto-fonts-ttf-variable
@@ -46,5 +43,9 @@ permit nopass :wheel cmd mount
 permit nopass :wheel cmd umount
 " > /etc/doas.conf
 chmod 400 /etc/doas.conf
+
+# Setup pipewire
+mkdir -p /etc/pipewire/pipewire.conf.d
+mkdir -p /etc/alsa/conf.d
 
 reboot
