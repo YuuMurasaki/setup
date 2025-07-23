8daaee07672e21c9c1e1d951a70e179d20f49e6e
Yuu Murasaki <hoangthibaongoc411@gmail.com>
2025-07-23 15:32:51 +0700

    hehe

--- extra.js
+++ extra.js
@@ -12,9 +12,11 @@ user_pref("extensions.pocket.enabled", false);
 // Disable screenshot extension
 user_pref("extensions.screenshots.disabled", true);
 
-// Disable advertisements in the search bar:
+// Disable crap in the search bar:
 user_pref("browser.urlbar.quicksuggest.enabled", false);
 user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
 
 // Disable preloading new tabs:
 user_pref("browser.newtab.preload", false);
@@ -48,10 +50,25 @@ user_pref("extensions.abuseReport.enabled", false);
 
 // Force DNS over HTTPS
 user_pref("network.trr.mode", 3);
+user_pref("network.trr.uri", "https://base.dns.mullvad.net/dns-query");
 
 // Disable WebRTC
 user_pref("media.peerconnection.enabled", false);
 
 // Disable bookmarks
 user_pref("browser.bookmarks.enabled", false);
-user_pref("browser.toolbars.bookmarks.visibility", never);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+// Enforce certificate pinning
+user_pref("security.cert_pinning.enforcement_level", 2);
+
+// Delete all browsing data on shutdown
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.clearOnShutdown_v2.cache", true);
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", true);
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearOnShutdown_v2.downloads", true); // [HIDDEN]
+user_pref("privacy.clearOnShutdown_v2.formdata", true);
+
+// After crashes or restarts, do not save extra session data
+user_pref("browser.sessionstore.privacy_level", 2);
