7ae7af3b2c29409d4bc16e5acd9cde55791b0636
Yuu Murasaki <hoangthibaongoc411@gmail.com>
2025-08-11 21:36:00 +0700

    make install script look better
    by follow LABRS, I make my install script better

--- configuration.sh
+++ configuration.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 # Enable sv services
+echo "Removing existing services..."
 rm -r /var/service/dhcpcd
 rm -r /var/service/agetty-tty4
 rm -r /var/service/agetty-tty5
 rm -r /var/service/agetty-tty6
 
+echo "Linking new services..."
 ln -s /etc/sv/NetworkManager/ /var/service/
 ln -s /etc/sv/rtkit/ /var/service/
 ln -s /etc/sv/dbus/ /var/service/
 
 # Setup $XDG_RUNTIME_DIR
+echo "Setting up XDG_RUNTIME_DIR..."
 echo "-session    optional    pam_rundir.so" >> /etc/pam.d/login
 
 # Setup doas
+echo "Configuring doas..."
 touch /etc/doas.conf
 echo "
 permit persist :wheel
@@ -23,11 +27,14 @@ permit nopass :wheel cmd mount
 permit nopass :wheel cmd umount
 " > /etc/doas.conf
 chmod 400 /etc/doas.conf
+echo "doas configuration complete."
 
 # Setup pipewire
+echo "Setting up PipeWire..."
 mkdir -p /etc/pipewire/pipewire.conf.d
 mkdir -p /etc/alsa/conf.d
 ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
 ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+echo "PipeWire setup complete."
--- install-packages.sh
+++ install-packages.sh
@@ -1,19 +1,45 @@
 #!/bin/sh
 
+gitmakeinstall() {
+    echo "Cloning '$1'..."
+    local dir=$(mktemp -d)
+    git clone --depth 1 --single-branch --no-tags -q "$1" "$dir"
+    cd "$dir" || exit 1
+    make clean install >/dev/null 2>&1
+    rm -rf "$dir"
+    echo "Done!"
+}
+
+install_font() {
+    echo "Installing fonts..."
+    local dir=$(mktemp -d)
+    cd "$dir" || exit
+    curl -OLs "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz"
+    tar -xf JetBrainsMono.tar.xz >/dev/null 2>&1
+    mv -- *.ttf /usr/share/fonts/TTF
+    rm -rf "$dir"
+    echo "Done!"
+}
+
+install_dotfiles() {
+    sudo -u "yuu" mkdir -p "/home/yuu/dotfiles"
+    sudo -u "yuu" \
+        git clone --single-branch --no-tags -q \
+        "https://github.com/YuuMurasaki/dotfiles.git" "/home/yuu/dotfiles"
+}
+
+# Main
+echo "Installing packages..."
 xbps-install -Syu
-xbps-install -y xorg-server xorg-apps xorg-input-drivers xorg-video-drivers
-xbps-install -y xcompmgr xdotool xclip xwallpaper unclutter-xfixes
-xbps-install -y vulkan-loader mesa-vulkan-intel
-xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel libXrandr-devel ncurses
-xbps-install -y freetype-devel fontconfig-devel xdg-utils xdg-user-dirs
-xbps-install -y dbus dbus-x11 pam_rundir NetworkManager
-xbps-install -y pipewire wireplumber rtkit alsa-pipewire
-xbps-install -y git curl wget ImageMagick ffmpeg
-xbps-install -y stow slop zathura-pdf-poppler
-xbps-install -y fzf rsync tar zip unzip
-xbps-install -y font-iosevka freefont-ttf dejavu-fonts-ttf ttf-ubuntu-font-family
-xbps-install -y liberation-fonts-ttf font-libertine-ttf font-awesome6
-xbps-install -y noto-fonts-emoji noto-fonts-cjk noto-fonts-cjk-variable noto-fonts-ttf noto-fonts-ttf-extra noto-fonts-ttf-variable
-xbps-install -y neovim lf mpv opendoas nsxiv shotgun zathura firefox keepassxc
+while IFS= read -r package; do
+    xbps-install -y "$package"
+done < packages.txt
+
+gitmakeinstall https://github.com/YuuMurasaki/st.git
+gitmakeinstall https://github.com/YuuMurasaki/dwm.git
+gitmakeinstall https://github.com/YuuMurasaki/dmenu.git
+
+install_font
+install_dotfiles
 
 reboot
