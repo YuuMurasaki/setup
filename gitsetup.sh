db4f2fc08f16f1e0b2f36a90afcd7e5dc1927a55
Yuu Murasaki <yuu@murasaki.com>
2025-08-15 23:19:28 +0700

    feat: add more git config

--- gitsetup.sh
+++ gitsetup.sh
@@ -6,8 +6,12 @@ echo -n "Email: " && read email
 git config --global user.name "$name"
 git config --global user.email "$email"
 git config --global core.editor "nvim"
+git config --global core.compression 0
+git config --global core.whitespace error
 git config --global color.ui auto
+git config --global diff.context 3
+git config --global interactive.singlekey true
 git config --global init.defaultBranch master
-git config --global commit.template "/home/yuu/.config/git/git-commit-template"
+git config --global commit.template "/home/yuu/.config/git/commit-template"
 
 echo "Git configuration completed successfully."
