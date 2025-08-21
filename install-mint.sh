f1e29eab65464b080cffbda7c5b015c035b8175d
Yuu Murasaki <yuu@murasaki.com>
2025-08-21 15:07:13 +0700

    fix install-mint script

--- install-mint.sh
+++ install-mint.sh
@@ -4,16 +4,18 @@
 sudo apt-get update && apt-get upgrade
 
 # Fonts
-sudo apt-get install -y fonts-recommended fonts-noto fonts-hack
-sudo apt-get install -y fonts-freefont-otf fonts-roboto fonts-dejavu
+sudo apt-get install -y fonts-recommended fonts-noto fonts-roboto
+sudo apt-get install -y fonts-freefont-otf fonts-dejavu
 sudo apt-get install -y fonts-linuxlibertine fonts-liberation
 sudo apt-get install -y fonts-crosextra-carlito fonts-crosextra-caladea
 
 # Software
 sudo apt-get install -y keepassxc audacious
 
-# Mullvad browser
-wget --trust-server-names https://mullvad.net/en/download/browser/linux-x86_64/latest -P ~/Downloads
+cd "$HOME/Downloads"
+wget --quiet https://mullvad.net/en/download/browser/linux-x86_64/latest
+wget --quiet https://download.virtualbox.org/virtualbox/7.2.0/virtualbox-7.2_7.2.0-170228~Ubuntu~noble_amd64.deb
+wget --quiet https://github.com/FreeTubeApp/FreeTube/releases/download/v0.23.7-beta/freetube_0.23.7_beta_amd64.deb
 
 # Final step
 sudo reboot
