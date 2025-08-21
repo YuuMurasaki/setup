dcea963848f29114f527d6e42f6c5d52b3c90437
Yuu Murasaki <yuu@murasaki.com>
2025-08-21 21:32:00 +0700

    Hehe

--- install-void.sh
+++ install-void.sh
@@ -23,7 +23,6 @@ sudo -u "yuu" mkdir -p /home/yuu/.ssh
 sudo -u "yuu" mkdir -p /home/yuu/workspace/testing
 sudo -u "yuu" mkdir -p /home/yuu/workspace/repos
 sudo -u "yuu" mkdir -p /home/yuu/pictures/screenshots
-sudo -u "yuu" mkdir -p /home/yuu/music
 
 cd /home/yuu
 sudo -u "yuu" git clone --depth 1 -q "https://github.com/YuuMurasaki/dotfiles.git"
