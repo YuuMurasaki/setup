8b710f42df8241ea1179b39cb2a53b6165ec8834
Yuu Murasaki <yuu@murasaki.com>
2025-08-24 14:44:30 +0700

    hehe

--- install-mint.sh
+++ install-mint.sh
@@ -16,6 +16,3 @@ cd "$HOME/Downloads"
 wget --quiet https://mullvad.net/en/download/browser/linux-x86_64/latest
 wget --quiet https://download.virtualbox.org/virtualbox/7.2.0/virtualbox-7.2_7.2.0-170228~Ubuntu~noble_amd64.deb
 wget --quiet https://github.com/FreeTubeApp/FreeTube/releases/download/v0.23.7-beta/freetube_0.23.7_beta_amd64.deb
-
-# Final step
-sudo reboot
--- install-void.sh
+++ install-void.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 xbps-install -Syu
-xbps-install -y xorg-server xorg-apps xorg-input-drivers xorg-video-drivers
-xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel libXrandr-devel ncurses
-xbps-install -y vulkan-loader mesa-vulkan-intel freetype-devel fontconfig-devel
-xbps-install -y xcompmgr xdotool xclip xwallpaper unclutter-xfixes
+xbps-install -y xorg-server xorg-apps xorg-input-drivers xorg-video-drivers ncurses
+xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel libXrandr-devel
+xbps-install -y freetype-devel fontconfig-devel NetworkManager
 xbps-install -y exfat-utils xdg-user-dirs dbus dbus-x11 pam_rundir
 xbps-install -y curl wget ImageMagick ffmpeg slop zathura-pdf-poppler man-db
-xbps-install -y NetworkManager pipewire wireplumber alsa-pipewire rtkit
+xbps-install -y xcompmgr xdotool xclip xwallpaper unclutter-xfixes
+xbps-install -y pipewire wireplumber alsa-pipewire rtkit
 xbps-install -y stow fzf rsync tar zip unzip lf
 xbps-install -y font-iosevka freefont-ttf ttf-ubuntu-font-family liberation-fonts-ttf font-libertine-ttf font-awesome6
 xbps-install -y noto-fonts-emoji noto-fonts-cjk noto-fonts-cjk-variable noto-fonts-ttf noto-fonts-ttf-extra noto-fonts-ttf-variable
-xbps-install -y tmux neovim mpv opendoas nsxiv shotgun zathura
+xbps-install -y tmux neovim mpv nsxiv shotgun zathura opendoas
 xbps-install -y firefox keepassxc
 
 cd /tmp
