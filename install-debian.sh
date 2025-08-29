5678d8a9c236ba7e9d21402d203bfdbee45a1ad2
Yuu Murasaki <hoangthibaongoc411@gmail.com>
2025-08-29 09:52:15 +0700

    fix debian install script

--- install-debian.sh
+++ install-debian.sh
@@ -3,9 +3,6 @@
 # Update system
 sudo apt-get update && apt-get upgrade
 
-# Tools
-sudo apt-get install -y rsync build-essential git
-
 # Fonts
 sudo apt-get install -y fonts-noto fonts-roboto fonts-firacode
 sudo apt-get install -y fonts-freefont-otf fonts-dejavu
@@ -14,7 +11,6 @@ sudo apt-get install -y fonts-linuxlibertine fonts-liberation
 # Software
 sudo apt-get install -y keepassxc audacious geany
 
-cd "$HOME/Downloads"
-wget --quiet https://mullvad.net/en/download/browser/linux-x86_64/latest
+wget --trust-server-names https://mullvad.net/en/download/browser/linux-x86_64/latest -P ~/Downloads
 #wget --quiet https://github.com/FreeTubeApp/FreeTube/releases/download/v0.23.7-beta/freetube_0.23.7_beta_amd64.deb
 #wget --quiet https://download.virtualbox.org/virtualbox/7.2.0/virtualbox-7.2_7.2.0-170228~Ubuntu~noble_amd64.deb
