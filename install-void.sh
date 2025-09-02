daccbd57b7c67501c722a77ef1be5fc1d0178514
Yuu Murasaki <yuu@murasaki.com>
2025-09-02 21:08:13 +0700

    hehe

--- install-void.sh
+++ install-void.sh
@@ -4,7 +4,7 @@ xbps-install -Syu
 xbps-install -y xorg-server xorg-apps xorg-input-drivers xorg-video-drivers ncurses
 xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel libXrandr-devel
 xbps-install -y freetype-devel fontconfig-devel NetworkManager
-xbps-install -y exfat-utils xdg-user-dirs dbus dbus-x11 pam_rundir
+xbps-install -y exfat-utils dbus dbus-x11 pam_rundir
 xbps-install -y curl wget ImageMagick ffmpeg zathura-pdf-poppler
 xbps-install -y xcompmgr xdotool xclip xwallpaper unclutter-xfixes
 xbps-install -y pipewire wireplumber alsa-pipewire rtkit
@@ -15,7 +15,7 @@ xbps-install -y noto-fonts-ttf-extra noto-fonts-cjk-variable noto-fonts-ttf-vari
 xbps-install -y liberation-fonts-ttf font-libertine-ttf freefont-ttf
 xbps-install -y tmux neovim mpv nsxiv zathura opendoas
 xbps-install -y firefox keepassxc
-#xbps-install -y vulkan-loader mesa-vulkan-intel slop shotgun
+#xbps-install -y vulkan-loader mesa-vulkan-intel slop shotgun xdg-user-dirs
 
 cd /tmp
 git clone --depth 1 --single-branch --no-tags -q "https://github.com/YuuMurasaki/st.git"
@@ -23,8 +23,8 @@ git clone --depth 1 --single-branch --no-tags -q "https://github.com/YuuMurasaki
 git clone --depth 1 --single-branch --no-tags -q "https://github.com/YuuMurasaki/dmenu.git"
 
 sudo -u "yuu" mkdir -p /home/yuu/.ssh
-sudo -u "yuu" mkdir -p /home/yuu/workspace/testing
 sudo -u "yuu" mkdir -p /home/yuu/workspace/repos
+sudo -u "yuu" mkdir -p /home/yuu/workspace/testing
 
 cd /home/yuu
 sudo -u "yuu" git clone --depth 1 -q "https://github.com/YuuMurasaki/dotfiles.git"
