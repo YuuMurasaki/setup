bef3a59a3d0cae598944cdcf8017b86955c345fb
Yuu Murasaki <yuu@murasaki.com>
2025-09-08 22:23:16 +0700

    asdf

--- fontsetup.sh
+++ fontsetup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-FONTDIR="$HOME/.local/share/fonts"
-mkdir -p "$FONTDIR"
-cd "$FONTDIR" || exit
+TEMP=$(mktemp -d)
+cd "$TEMP" || exit
 
 curl -OLs "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz"
 tar -xJf JetBrainsMono.tar.xz
+
+FONT_DIR="/usr/share/fonts/TTF/"
+mv -- *.ttf "$FONT_DIR"
