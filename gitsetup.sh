5f77d2a3ad5837b97ee7572e1d4f20834e08affe
Yuu Murasaki <yuu@murasaki.com>
2025-11-07 20:06:04 +0700

    hehe

--- gitsetup.sh
+++ gitsetup.sh
@@ -11,5 +11,6 @@ git config --global core.whitespace error
 git config --global color.ui auto
 git config --global diff.context 3
 git config --global init.defaultBranch master
+git config --global commit.template "/home/yuu/.config/git/commit-template"
 
 echo "Git configuration completed successfully."
